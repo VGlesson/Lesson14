04a38ba24b8982764bb1ca0be8bce85b262c0819
Владимир Гаврилов <saxar.1kg@mail.ru>
2022-11-02 17:09:40 +0300

    Добавьте файлы проекта.

---
+++ App1/Animal.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public Animal (string name)
+        {
+            Name = name;
+        }
+        public abstract void GetSay();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            GetSay();
+        }
+
+    }
+
+    class Cat : Animal
+    {
+        string say;
+        string name;
+        public override string Name 
+        { get => name; 
+          set =>name = value; 
+        }
+
+        public Cat(string name, string say)
+            :base(name)
+        {
+            this.say = say;
+        }
+
+        public Cat(string name) : base(name)
+        {
+        }
+
+        public override void GetSay()
+        {
+            Console.WriteLine("мяу");
+        }
+    }
+    class Dog : Animal
+    {
+        string say;
+        string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Dog(string name, string say)
+            : base(name)
+        {
+            this.say = say;
+        }
+
+        public Dog(string name) : base(name)
+        {
+        }
+
+        public override void GetSay()
+        {
+            Console.WriteLine("гав");
+        }
+    }
+}
---
+++ App1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Мурка");
+            Console.WriteLine(cat); 
+            Dog dog = new Dog("Тузик");
+            Console.WriteLine(dog);
+            Console.ReadKey();
+        }
+    }
+}
